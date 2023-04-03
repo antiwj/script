54dfe59707c9d00a0676fa14db455952df5fc077
antiwj <127014396+antiwj@users.noreply.github.com>
2023-04-03 16:59:22 +0800

    Update ufw.sh

--- ufw.sh
+++ ufw.sh
@@ -76,5 +76,5 @@ mkdir -p /root/.ssh
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDI3I3lZleT0wkrBwUWBNXhdecpLdF91AgeEKWDKaXiNOrg6DAe9tyOfpFIaxx1ek+H7Qc+TzShY/WUG/D7STlamWP6lKiV8U3WQkhq8ez0rVkXaDsv7RbaGpmbrENZCGrIJIVu4qk7xDkzxIaG6hJCiR+/HVGkwT+nHvAeYDhHo7fqXaYD0TeMpL4dfnZkUtzzNO+LK95ggQX4a69VTAy7dEihaNZ4ENcK5IBdrydJDafVL/Uo8NEDEaSYlUv9WKIb5dpmqJyvXPbfPh/pntGlrJGlItWZBIp7tPD3B4j4JRF4nkegPGorDDLEdmk/B3L4r11uW4ssC16VA2YLaHiN root@requactblack.ga' >> /root/.ssh/authorized_keys
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDVly2zvIA9bgGZF6dxxSp9N1mA8XPFNnTujj35OsIUpUgMH/EKjKw8/VZbHtdzrgvFnvCL/C0OTpSZSeXw/azlorSfCTP2xlww0IgzJaboRKZLqDvU9QNRQPBoihzIxEIqH0a4G6bEWQtq1ajevdGPBuljnmMRYxnOYpLJf4Lw+h87sNIRnjNg8Actf7RHE6Xp6m9AvhO7YqkvvdixTuJK8H9h/tZIDfJfbU5m9uFPS6hJQQbyc1T0wTMknjTGNkjz9a2fi9+ZPtEHpVZoUC2aE02SP/ZtQxRLI6fN03C5EWQTjRFXx2EjIh3470C59tJUIrg3i8AsW26f32EdIQVD root@requactblack.ga' >> /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
-echo 'PermitRootLogin prohibitpassword' >> /etc/ssh/sshd_config
+echo 'PermitRootLogin prohibit-password' >> /etc/ssh/sshd_config
 systemctl restart sshd
